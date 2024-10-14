4c1a2581574ee314cba2c193853d4bcf129766aa
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2024-10-15 02:48:05 +0530

    update insertion sort

--- sorting.c
+++ sorting.c
@@ -1,5 +1,7 @@
-void bubbleSort (int arr[]) {
-    int len = sizeof(arr) / sizeof(int);
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void bubbleSort (int arr[], int len) {
     for(int i = 0; i < len - 1; i++){
         for(int j = 0; j < len - 1 - i; j++ ){
             if( arr[j] > arr[j + 1]){
@@ -9,5 +11,16 @@ void bubbleSort (int arr[]) {
                 arr[j + 1] = temp;
             }
         }
+    }
+}
+
+void insetionSort (int arr[], int len){
+    for(int i = 1; i < len; i++){
+        int temp = arr[i];
+        int j;
+        for(j = i; j > 0 && temp < arr[j - 1]; j--){
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = temp;
     }
 }
